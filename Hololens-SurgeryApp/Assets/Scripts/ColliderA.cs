a7871e1845cec7da772c46fa63170a93849673c4
Anthony Moore <amooretmp@gmail.com>
2020-02-18 12:37:01 +0000

    added holo project
    3D hologram, Images via http

---
+++ Hololens-SurgeryApp/Assets/Scripts/ColliderA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderA : MonoBehaviour
+{
+    public bool isTaggedA = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name=="Jig")
+        {
+            Debug.Log("Collision A:");
+            isTaggedA = true; 
+        }  
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if(other.gameObject.name=="Jig")
+        {
+            Debug.Log("Exit A:");
+            isTaggedA = false;  
+        }
+    }
+}
